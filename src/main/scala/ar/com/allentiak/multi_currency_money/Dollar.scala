a9f556c12cd3c9d20e6d52bec646961489020cb4
Leandro Doctors <ldoctors@gmail.com>
2016-06-22 00:53:16 -0300

    test: equality: amount should be private (does not compile)

--- src/main/scala/ar/com/allentiak/multi_currency_money/Dollar.scala
+++ src/main/scala/ar/com/allentiak/multi_currency_money/Dollar.scala
@@ -1,6 +1,6 @@
 package ar.com.allentiak.multi_currency_money
 
-class Dollar(val amount: Int){
+class Dollar(private val amount: Int){
   def times(multiplier: Int) = new Dollar(amount * multiplier)
   override def equals(other: Any) = other match {
     case o:Dollar => amount == o.amount
