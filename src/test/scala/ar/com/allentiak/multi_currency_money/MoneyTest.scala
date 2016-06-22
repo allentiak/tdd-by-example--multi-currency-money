0ba44eefd0c1ac689fdae48abc1564e1dddba1a8
Leandro Doctors <ldoctors@gmail.com>
2016-06-22 00:55:09 -0300

    test: equality: amount should be private (now it works)

--- src/test/scala/ar/com/allentiak/multi_currency_money/MoneyTest.scala
+++ src/test/scala/ar/com/allentiak/multi_currency_money/MoneyTest.scala
@@ -6,8 +6,8 @@ class MoneyTest extends FunSpec {
   describe("Multi-Currency Money") {
     it("should support multiplication by a real number") {
       val five = new Dollar(5)
-      assert(five.times(2).amount === 10)
-      assert(five.times(3).amount === 15)
+      assert(five.times(2) === new Dollar(10))
+      assert(five.times(3) === new Dollar(15))
     }
     it("should support equality") {
       assert(new Dollar(5).equals(new Dollar(5)))
