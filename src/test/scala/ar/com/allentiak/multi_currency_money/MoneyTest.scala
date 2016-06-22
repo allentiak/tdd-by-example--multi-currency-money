7b02db6870b10074a8ecf44f9bdcbacb2537ffc4
Leandro Doctors <ldoctors@gmail.com>
2016-06-22 15:19:18 -0300

    Dollar-Franc deduplication: use a factory method for Dollar (test fails)

--- src/test/scala/ar/com/allentiak/multi_currency_money/MoneyTest.scala
+++ src/test/scala/ar/com/allentiak/multi_currency_money/MoneyTest.scala
@@ -7,9 +7,9 @@ class MoneyTest extends FunSpec {
   describe("Multi-Currency-enabled Money") {
 
     it("should support multiplication by a real number") {
-      val fiveDollars = new Dollar(5)
-      assert(fiveDollars.times(2) === new Dollar(10))
-      assert(fiveDollars.times(3) === new Dollar(15))
+      val fiveDollars = Money.dollar(5)
+      assert(fiveDollars.times(2) === Money.dollar(10))
+      assert(fiveDollars.times(3) === Money.dollar(15))
       val fiveFrancs = new Franc(5)
       assert(fiveFrancs.times(2) === new Franc(10))
       assert(fiveFrancs.times(3) === new Franc(15))
