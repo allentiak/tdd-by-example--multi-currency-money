c13c223d6568e8b9f07c3e89c78406adac4997f6
Leandro Doctors <ldoctors@gmail.com>
2016-06-22 00:23:05 -0300

    test: equality: make test work via triangulation (generalize implementation)

--- src/main/scala/ar/com/allentiak/multi_currency_money/Dollar.scala
+++ src/main/scala/ar/com/allentiak/multi_currency_money/Dollar.scala
@@ -2,5 +2,7 @@ package ar.com.allentiak.multi_currency_money
 
 class Dollar(val amount: Int){
   def times(multiplier: Int) = new Dollar(amount * multiplier)
-  override def equals(other: Any) = true
+  override def equals(other: Any) = other match {
+    case o:Dollar => amount == o.amount
+  }
 }
