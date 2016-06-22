b2a0b3f9b5a884f0028d3eebc10248c98c53f042
Leandro Doctors <ldoctors@gmail.com>
2016-06-21 23:54:39 -0300

    test: equality: force failure (class <- case class)

--- src/main/scala/ar/com/allentiak/multi_currency_money/Dollar.scala
+++ src/main/scala/ar/com/allentiak/multi_currency_money/Dollar.scala
@@ -1,5 +1,5 @@
 package ar.com.allentiak.multi_currency_money
 
-case class Dollar(amount: Int){
-  def times(multiplier: Int) = Dollar(amount * multiplier)
+class Dollar(val amount: Int){
+  def times(multiplier: Int) = new Dollar(amount * multiplier)
 }
--- src/test/scala/ar/com/allentiak/multi_currency_money/MoneyTest.scala
+++ src/test/scala/ar/com/allentiak/multi_currency_money/MoneyTest.scala
@@ -5,12 +5,12 @@ import org.scalatest.FunSpec
 class MoneyTest extends FunSpec {
   describe("Multi-Currency Money") {
     it("should support multiplication by a real number") {
-      val five = Dollar(5)
+      val five = new Dollar(5)
       assert(five.times(2).amount === 10)
       assert(five.times(3).amount === 15)
     }
     it("should support equality") {
-      assert(Dollar(5).equals(Dollar(5)))
+      assert(new Dollar(5).equals(new Dollar(5)))
     }
   }
 }
