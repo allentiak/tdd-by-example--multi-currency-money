02c85074f3cc9d939c64fa5ad7008535307b3860
Leandro Doctors <ldoctors@gmail.com>
2016-06-23 16:10:39 -0300

    Money.plus should return a Sum (test fails)

--- src/main/scala/ar/com/allentiak/multi_currency_money/Money.scala
+++ src/main/scala/ar/com/allentiak/multi_currency_money/Money.scala
@@ -2,6 +2,8 @@ package ar.com.allentiak.multi_currency_money
 
 trait Expression
 
+class Sum (val augend: Money, val addend: Money) extends Expression
+
 class Bank {
   def reduce(source: Expression, to: String): Money = Money.dollar(10)
 }
@@ -16,7 +18,7 @@ class Money (protected val amount: Int, val currency: String) extends Expression
   }
   def times(multiplier: Int): Money = new Money(amount * multiplier, currency)
   override def toString: String = amount + " " + currency
-  def plus(other:Money): Expression = new Money(amount + other.amount, currency)
+  def plus(addend:Money): Expression = new Sum(this, addend)
 }
 
 object Money {
